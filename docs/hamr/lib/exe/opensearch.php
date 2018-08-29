7fc69e43e8049a0a87d3d64f6dbb98f673bb9aa3
Eric Humphrey <ejhumphrey@spotify.com>
2018-08-29 09:10:22 -0400

    Added everything but the meat

---
+++ docs/hamr/lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>HAMR@ISMIR 2017 Proceedings</ShortName>
+  <Image width="16" height="16" type="image/x-icon">https://ismir2017.smcnus.org/hamr/lib/tpl/dokuwiki/images/favicon.ico</Image>
+  <Url type="text/html" template="https://ismir2017.smcnus.org/hamr/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="https://ismir2017.smcnus.org/hamr/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
